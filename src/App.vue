484740b05fc2760bcea8a7d2b6002a87e7fa7bb4
Veronica <veronica.alcala@gmail.com>
2021-06-14 16:20:46 +0200

    vue props

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{title}}</h1>
+    <!-- <input type="text" ref="name">
+    <button @click="handleClick">click me</button> -->
+    <Modal :header='header' :text='text' theme='sale' />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Modal
+  },
+  data() {
+    return {
+      title: 'My first Vue App :) !',
+      header: 'This is the header prop',
+      text: 'This is the text prop'
+    }
+  },
+  // methods: {
+  //   handleClick() {
+  //     console.log(this.$refs.name)
+  //     this.$refs.name.classList.add('active')
+  //   }
+  // }
 }
 </script>
 
@@ -22,5 +37,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
